1d96b0c15b218e0701c6e4a3026724ca7252caec
Snehal <singhsnehal.2k1@gmail.com>
2025-08-17 18:01:31 +0530

    Async functionality - executing sync ops like db insert and db search in new threads - run_in_threadpool function from fastapi.concurrency

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path, Request
+from fastapi.concurrency import run_in_threadpool
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -24,18 +25,18 @@ def getRoot(request: Request):
 
 
 @app.post('/api/v1/create/')
-def createUrl(long_url: HttpURL):
-    short_url = insert_value(long_url.url)
+async def createUrl(long_url: HttpURL):
+    short_url = await run_in_threadpool(insert_value, long_url.url)
     return {'short_url': short_url}
 
 
 @app.get('/{short_url}')
-def urlFetch(short_url: str = Path(
+async def urlFetch(short_url: str = Path(
         description='Short URL path',
         pattern='^[a-zA-Z0-9]{5}$'
         ), info: bool = False
     ):
-    long_url, c_date = get_long_url(short_url)
+    long_url, c_date = await run_in_threadpool(get_long_url, short_url)
     if info:
         return {"short_url":short_url, "long_url": long_url, "c_date": c_date.strftime("%Y-%m-%d %H:%M:%S")}
     return RedirectResponse(long_url)
